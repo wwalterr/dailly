b10541ddaea138c23d35a460c67d141f74cf4b3e
Sphinxs <whenrique26@gmail.com>
2021-04-25 13:33:39 -0300

    Emojis setup

--- screns/tasks.js
+++ screns/tasks.js
@@ -2,7 +2,11 @@ import React from "react";
 
 import { View, Text, StyleSheet } from "react-native";
 
+import Emoji from "react-native-emoji";
+
 import { useTasks } from "../contexts/tasks";
+
+import emojis from "../utils/emojis";
 
 const Tasks = () => {
   const { tasks, addTask, updateTask, removeTask } = useTasks();
@@ -12,6 +16,8 @@ const Tasks = () => {
       <Text>Tasks</Text>
 
       <Text>{JSON.stringify(tasks)}</Text>
+
+      <Emoji name={emojis[0].aliases} style={{ fontSize: 50 }} />
     </View>
   );
 };
