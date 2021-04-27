051de664a1e37a84e10a725d68ed4bf8647dd101
Sphinxs <whenrique26@gmail.com>
2021-04-27 17:12:14 -0300

    Animation to tasks list

--- components/task.js
+++ components/task.js
@@ -6,12 +6,22 @@ import {
   TextInput,
   Switch,
   Button,
+  Dimensions,
+  Animated,
   StyleSheet,
 } from "react-native";
 
+import theme from "../theme";
+
 import { useTasks } from "../contexts/tasks";
 
-const Task = ({ task }) => {
+const HEIGHT = Dimensions.get("window").height - 64;
+
+const TASK_MARGIN = 16;
+
+const TASK_HEIGHT = 50 + TASK_MARGIN * 2;
+
+const Task = ({ task, index, y }) => {
   const { removeTask, updateTask } = useTasks();
 
   const [text, setText] = useState(task.text);
@@ -22,8 +32,47 @@ const Task = ({ task }) => {
 
   const [counter, setCounter] = useState(task.counter ? task.counter : 0);
 
+  const position = Animated.subtract(index * TASK_HEIGHT, y);
+
+  const isDisappearing = -TASK_HEIGHT;
+
+  const isAppearing = HEIGHT;
+
+  const isTop = 0;
+
+  const isBottom = HEIGHT - TASK_HEIGHT;
+
+  const translateY = Animated.add(
+    y,
+    y.interpolate({
+      inputRange: [0, 0.00001 + index * TASK_HEIGHT],
+      outputRange: [0, -index * (TASK_HEIGHT + 2 * TASK_MARGIN)],
+      extrapolateRight: "clamp",
+    })
+  );
+
+  const scale = position.interpolate({
+    inputRange: [isDisappearing, isTop, isBottom, isAppearing],
+    outputRange: [0.5, 1, 1, 0.5],
+    extrapolateRight: "clamp",
+  });
+
+  const opacity = position.interpolate({
+    inputRange: [isDisappearing, isTop, isBottom, isAppearing],
+    outputRange: [0.5, 1, 1, 0.5],
+  });
+
   return (
-    <View style={styles.container}>
+    <Animated.View
+      style={[
+        styles.container,
+        {
+          height: TASK_HEIGHT,
+          opacity,
+          transform: [{ translateY }, { scale }],
+        },
+      ]}
+    >
       <TextInput onChangeText={setText} value={text} />
 
       {/* <Emoji name={emojis[0].aliases[0]} style={{ fontSize: 50 }} /> */}
@@ -68,14 +117,15 @@ const Task = ({ task }) => {
         color="#841584"
         onPress={() => removeTask(task.id)}
       /> */}
-    </View>
+    </Animated.View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    // backgroundColor: "#f00",
-    margin: 24,
+    marginHorizontal: 32,
+    marginVertical: TASK_MARGIN,
+    backgroundColor: theme.color.gray.light,
   },
 });
 
--- components/tasks.js
+++ components/tasks.js
@@ -1,22 +1,43 @@
 import React from "react";
 
-import { FlatList, StyleSheet } from "react-native";
+import { Animated, FlatList, StyleSheet } from "react-native";
 
 import { useTasks } from "../contexts/tasks";
 
 import Task from "./task";
 
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+
 const Tasks = () => {
   const { tasks } = useTasks();
 
+  const y = new Animated.Value(0);
+
+  const onScroll = Animated.event(
+    [
+      {
+        nativeEvent: {
+          contentOffset: {
+            y,
+          },
+        },
+      },
+    ],
+    {
+      useNativeDriver: true,
+    }
+  );
+
   return (
-    <FlatList
+    <AnimatedFlatList
       data={tasks}
-      renderItem={({ item, index }) => <Task task={item} />}
+      renderItem={({ item, index }) => <Task task={item} index={index} y={y} />}
       keyExtractor={(item, index) => item.id}
       showsHorizontalScrollIndicator={false}
       showsVerticalScrollIndicator={false}
       bounces={false}
+      scrollEventThrottle={16}
+      {...{ onScroll }}
       style={styles.flatList}
     />
   );
