02b3dd3f2eba66afccafbe2aee6354942141fb4f
Hydroxion <whenrique26@gmail.com>
2021-06-07 08:53:33 -0300

    Remove logs

--- components/metrics.js
+++ components/metrics.js
@@ -65,7 +65,6 @@ const Metrics = ({ task, showMetricsModal, setShowMetricsModal }) => {
   //
   // const height = 64;
 
-  console.log();
   return (
     <Modal
       isVisible={showMetricsModal}
