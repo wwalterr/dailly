4160a3377627791787b11d4a1274cfb0ab7efc5a
Hydroxion <whenrique26@gmail.com>
2021-07-19 20:17:01 -0300

    Fix unimodule conflict between direct and peer dependencies

--- android/app/src/main/java/com/dailly/dailly/generated/BasePackageList.java
+++ android/app/src/main/java/com/dailly/dailly/generated/BasePackageList.java
@@ -7,7 +7,6 @@ import org.unimodules.core.interfaces.Package;
 public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
-        new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.application.ApplicationPackage(),
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.errorrecovery.ErrorRecoveryPackage(),
@@ -16,7 +15,8 @@ public class BasePackageList {
         new expo.modules.imageloader.ImageLoaderPackage(),
         new expo.modules.keepawake.KeepAwakePackage(),
         new expo.modules.notifications.NotificationsPackage(),
-        new expo.modules.permissions.PermissionsPackage()
+        new expo.modules.permissions.PermissionsPackage(),
+        new expo.modules.splashscreen.SplashScreenPackage()
     );
   }
 }
