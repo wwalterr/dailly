fff6c6dc155b628420d08d0d977ddf84bb7e6b56
Hydroxion <whenrique26@gmail.com>
2021-08-19 14:53:39 -0300

    Fix app registry

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { registerRootComponent } from "expo";
+import { AppRegistry } from "react-native";
 
 import App from "./App";
 
-registerRootComponent(App);
+AppRegistry.registerComponent("main", () => App);
